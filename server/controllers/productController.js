8034517db3147f56f07fe6a6266690e7e22c31a5
Rohith-Manjunath <rohithmanjunath20@gmail.com>
2023-12-07 20:10:15 +0530

    Implement User Review Management
    In this commit:
    - Allow users to create reviews for products, providing ratings and comments
    - Get particular product reviews.
    - Enable users to update their existing reviews, modifying comments and ratings
    - Implement the ability for users to delete their own reviews for products
    - Enhance validation to ensure consistency and security in user reviews
    - Thoroughly test the new review management functionalities for reliability
    
    These enhancements empower users to have better control over their reviews, fostering a more interactive and engaging experience with the product catalog.

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -56,3 +56,84 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
   }
   res.status(200).json({ success: true, message: "Product created", product });
 });
+
+exports.createReview = catchAsyncErrors(async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
+  const { _id, name } = req.user;
+  const review = {
+    name: name,
+    rating: Number(rating),
+    comment: String(comment),
+    user: _id,
+  };
+  const product = await Product.findById(productId);
+
+  const isReviewed = product.reviews.find(
+    (rev) => rev.user.toString() === _id.toString()
+  );
+
+  if (isReviewed) {
+    product.reviews.find((rev) => {
+      if (rev.user.toString() === _id.toString()) {
+        (rev.comment = comment), (rev.rating = rating);
+      }
+    });
+  } else {
+    product.reviews.push(review);
+    product.numOfReviews = product.reviews.length;
+  }
+
+  // Calculate average rating
+  let totalAvg = 0;
+  product.reviews.forEach((rev) => {
+    totalAvg += rev.rating;
+  });
+
+  product.ratings = (totalAvg / product.reviews.length).toFixed(1);
+
+  await product.save();
+  res
+    .status(200)
+    .json({ success: true, message: "Review added successfully!" });
+});
+
+exports.singleProductReview = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+  if (!product) {
+    return next(new ErrorHandler("No reviews found", 404));
+  }
+  res
+    .status(200)
+    .json({ success: true, message: "Successful", reviews: product.reviews });
+});
+
+exports.deleteProductReview = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.query.productId);
+  if (!product) {
+    return next(new ErrorHandler("Product not Found", 404));
+  }
+
+  const reviews = product.reviews.filter(
+    (rev) => rev._id.toString() !== req.query.id.toString()
+  );
+
+  // Calculate average rating
+  let avg = 0;
+  reviews.forEach((rev) => {
+    avg += rev.rating;
+  });
+
+  const ratings = reviews.length > 0 ? avg / reviews.length : 0;
+  const numOfReviews = reviews.length;
+
+  await Product.findByIdAndUpdate(req.query.productId, {
+    reviews,
+    ratings,
+    numOfReviews,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Review deleted successfully!",
+  });
+});
