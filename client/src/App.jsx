ea51dd926349c3356cc32deeb92b664ba92f200f
Rohith-Manjunath <rohithmanjunath20@gmail.com>
2023-12-08 15:31:23 +0530

    style(navbar): implement initial navbar layout for desktop
    This commit introduces the initial version of the navbar component for desktop view in the e-commerce application. While the navbar provides the expected links and features, it is currently not responsive.
    
    Key features of this commit:
    - Added Home, Products, Contact, Cart, Profile and About links.
    - Customized styling for desktop layout.
    - Implemented Profile, Search, and Cart icons.
    
    Future work will focus on making the navbar fully responsive for various screen sizes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Search from "./pages/Search";
+import Cart from "./pages/Cart";
+import Profile from "./pages/Profile";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/products" element={<Home />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,76 @@
+import { Link, NavLink } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+import { IoBagHandleSharp } from "react-icons/io5";
+import { LuUserSquare } from "react-icons/lu";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-center p-2 bg-slate-500">
+      <div className="w-1/3">
+        <Link to={"/"}>
+          <h1 className="ecommerce text-4xl text-white font-semibold hover:cursor-pointer hover:text-green-400 transition-all duration-300">
+            Ecommerce
+          </h1>
+        </Link>
+      </div>
+      <div className="w-1/3 flex items-center justify-center">
+        <ul className="flex items-center justify-center gap-10 font-semibold text-white">
+          <li className="relative hover:text-green-400">
+            <NavLink
+              to={"/"}
+              className="after:block after:w-full after:h-[2px] after:bg-white after:scale-0 hover:after:scale-100 after:transition-all hover:origin-center duration-500 ease-in-out hover:scale-110 hover:after:bg-green-400"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="relative hover:text-green-400">
+            <NavLink
+              to={"/products"}
+              className="after:block after:w-full after:h-[2px] after:bg-white after:scale-0 hover:after:scale-100 after:transition-all hover:origin-center duration-500 ease-in-out hover:scale-110 hover:after:bg-green-400"
+            >
+              Products
+            </NavLink>
+          </li>
+          <li className="relative hover:text-green-400">
+            <NavLink
+              to={"/contact"}
+              className="after:block after:w-full after:h-[2px] after:bg-white after:scale-0 hover:after:scale-100 after:transition-all hover:origin-center duration-500 ease-in-out hover:scale-110 hover:after:bg-green-400"
+            >
+              Contact
+            </NavLink>
+          </li>
+          <li className="relative hover:text-green-400">
+            <NavLink
+              to={"/about"}
+              className="after:block after:w-full after:h-[2px] after:bg-white after:scale-0 hover:after:scale-100 after:transition-all hover:origin-center duration-500 ease-in-out hover:scale-110 hover:after:bg-green-400"
+            >
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="w-1/3 text-white flex items-center justify-center gap-5 text-xl hover:cursor-pointer">
+        <Link
+          to="/search"
+          className="hover:text-green-400 transition-all duration-300 hover:scale-110"
+        >
+          <FaSearch />
+        </Link>
+        <Link
+          to="/cart"
+          className="hover:text-green-400 transition-all duration-300 hover:scale-110"
+        >
+          <IoBagHandleSharp />
+        </Link>
+        <Link
+          to="/profile"
+          className="hover:text-green-400 transition-all duration-300 hover:scale-110"
+        >
+          <LuUserSquare />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
